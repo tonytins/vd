c7cfd45c35781e8747b079f1c038a6ddb5551851
Tony Bark <noreply@tonybark.com>
2021-04-30 04:31:29 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use clap::{AppSettings, Clap};
+use rand::distributions::{Distribution, Uniform};
+
+#[derive(Clap)]
+#[clap(author, about, version)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(short, long)]
+    roll: Option<i32>,
+    #[clap(short, long)]
+    custom: Option<i32>,
+}
+
+fn roll(dice: i32) -> i32 {
+    let mut rng = rand::thread_rng();
+    let range = Uniform::from(1..dice);
+
+    range.sample(&mut rng)
+}
+
+fn main() {
+    let opts: Opts = Opts::parse();
+
+    if opts.roll.is_some() {
+        match opts.roll.unwrap() {
+            4 => println!("{}", roll(4)),
+            6 => println!("{}", roll(6)),
+            8 => println!("{}", roll(8)),
+            10 => println!("{}", roll(10)),
+            12 => println!("{}", roll(12)),
+            _ => println!("{}", roll(20)),
+        }
+    } else if opts.custom.is_some() {
+        println!("{}", roll(opts.custom.unwrap()));
+    } else {
+        println!("{}", roll(20));
+    }
+}
